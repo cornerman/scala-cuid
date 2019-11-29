17ac6bce5adfb390bbf07ac632208152c0e81bc2
Scala Steward <me@scala-steward.org>
2019-11-29 13:17:10 +0100

    Update scalatest to 3.1.0

--- cuid/shared/src/test/scala/CuidSpec.scala
+++ cuid/shared/src/test/scala/CuidSpec.scala
@@ -1,8 +1,10 @@
 package cuid
 
 import org.scalatest._
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.must.Matchers
 
-class CuidSpec extends AsyncFreeSpec with MustMatchers {
+class CuidSpec extends AsyncFreeSpec with Matchers {
   "true" in {
     val cuid = Cuid()
     cuid.length must be > 0
