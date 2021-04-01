257964ed4582662dfdc761351d1a7f1378240f74
Scala Steward <me@scala-steward.org>
2021-04-01 10:21:00 +0200

    Update scalatest to 3.2.7

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,6 +6,6 @@ object Deps {
   // hack to expand %%% in settings, needs .value in build.sbt
   import Def.{setting => dep}
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.2")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.7")
   val cuidJava = dep("cool.graph" % "cuid-java" % "0.1.1")
 }
